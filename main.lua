ef6ab54495050e256e2e566555bae0dd05c305a5
Shikami <theshikami@gmail.com>
2024-12-19 13:41:37 +0330

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
-io.stdout:setvbuf("no")
+-- setting variables
 function love.load()
 x = 100
 y = 100
 end
-
+-- Moving the rectangle with the keyboard
 function love.update(dt)
   if love.keyboard.isDown('w') and love.keyboard.isDown('d') then
     x = x + 60 * dt
@@ -27,7 +27,7 @@ function love.update(dt)
     x = x + 60 * dt
   end
 end
-
+-- drawing the rectangle
 function love.draw()
   love.graphics.rectangle("line", x, y, 200, 150)
 end
